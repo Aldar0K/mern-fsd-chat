63f037905e6df22baf441a4771cb42bddce8b8b7
Aldar <aldarkonov21@gmail.com>
2023-04-23 18:01:58 +0300

    feat: update home page, add layouts for login and register forms

---
+++ frontend/src/components/LoginForm/LoginForm.tsx
@@ -0,0 +1,9 @@
+import { FC } from 'react';
+
+import styles from './LoginForm.module.scss';
+
+const LoginForm: FC = () => {
+  return <div className={styles.container}>LoginForm</div>;
+};
+
+export default LoginForm;
---
+++ frontend/src/components/SignupForm/SignupForm.tsx
@@ -0,0 +1,9 @@
+import { FC } from 'react';
+
+import styles from './SignupForm.module.scss';
+
+const SignupForm: FC = () => {
+  return <div className={styles.container}>SignupForm</div>;
+};
+
+export default SignupForm;
