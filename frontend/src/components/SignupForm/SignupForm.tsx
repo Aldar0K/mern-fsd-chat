679b86b17f836360147f167f2a2e7c18bef05e97
Aldar <aldarkonov21@gmail.com>
2023-04-24 20:55:12 +0300

    feat: add signup form

--- frontend/src/components/SignupForm/SignupForm.tsx
+++ frontend/src/components/SignupForm/SignupForm.tsx
@@ -1,9 +1,119 @@
-import { FC } from 'react';
-
-import styles from './SignupForm.module.scss';
+import {
+  Button,
+  FormControl,
+  FormLabel,
+  Input,
+  InputGroup,
+  InputRightElement,
+  VStack,
+  useToast
+} from '@chakra-ui/react';
+import { FC, useState } from 'react';
 
 const SignupForm: FC = () => {
-  return <div className={styles.container}>SignupForm</div>;
+  const toast = useToast();
+
+  const [name, setName] = useState<string>('');
+  const [email, setEmail] = useState<string>('');
+  const [confirmPassword, setConfirmPassword] = useState<string>('');
+  const [password, setPassword] = useState<string>('');
+  const [image, setImage] = useState<File | null>(null);
+
+  const [isLoading, setLoading] = useState<boolean>(false);
+  const [showPassword, setShowPassword] = useState<boolean>(false);
+  const toggleShowPassword = () => setShowPassword(prev => !prev);
+
+  const submitHandler = async () => {
+    setLoading(true);
+    if (!name || !email || !password || !confirmPassword) {
+      toast({
+        title: 'Please Fill all the Feilds',
+        status: 'warning',
+        duration: 5000,
+        isClosable: true,
+        position: 'bottom'
+      });
+      setLoading(false);
+      return;
+    }
+    if (password !== confirmPassword) {
+      toast({
+        title: 'Passwords Do Not Match',
+        status: 'warning',
+        duration: 5000,
+        isClosable: true,
+        position: 'bottom'
+      });
+      return;
+    }
+    console.log(name, email, password, image);
+    setLoading(false);
+  };
+
+  return (
+    <VStack spacing='5px'>
+      <FormControl id='first-name' isRequired>
+        <FormLabel>Name</FormLabel>
+        <Input placeholder='Enter Your Name' onChange={e => setName(e.target.value)} />
+      </FormControl>
+      <FormControl id='email' isRequired>
+        <FormLabel>Email Address</FormLabel>
+        <Input
+          type='email'
+          placeholder='Enter Your Email Address'
+          onChange={e => setEmail(e.target.value)}
+        />
+      </FormControl>
+      <FormControl id='password' isRequired>
+        <FormLabel>Password</FormLabel>
+        <InputGroup size='md'>
+          <Input
+            type={showPassword ? 'text' : 'password'}
+            placeholder='Enter Password'
+            onChange={e => setPassword(e.target.value)}
+          />
+          <InputRightElement width='4.5rem'>
+            <Button h='1.75rem' size='sm' onClick={toggleShowPassword}>
+              {showPassword ? 'Hide' : 'Show'}
+            </Button>
+          </InputRightElement>
+        </InputGroup>
+      </FormControl>
+      <FormControl id='confirm-password' isRequired>
+        <FormLabel>Confirm Password</FormLabel>
+        <InputGroup size='md'>
+          <Input
+            type={showPassword ? 'text' : 'password'}
+            placeholder='Confirm password'
+            onChange={e => setConfirmPassword(e.target.value)}
+          />
+          <InputRightElement width='4.5rem'>
+            <Button h='1.75rem' size='sm' onClick={toggleShowPassword}>
+              {showPassword ? 'Hide' : 'Show'}
+            </Button>
+          </InputRightElement>
+        </InputGroup>
+      </FormControl>
+      <FormControl id='image'>
+        <FormLabel>Upload your Picture</FormLabel>
+        <Input
+          type='file'
+          p={1.5}
+          accept='image/*'
+          onChange={e => e.target.files && setImage(e.target.files[0])}
+        />
+      </FormControl>
+      <Button
+        colorScheme='blue'
+        width='100%'
+        style={{ marginTop: 15 }}
+        onClick={submitHandler}
+        isLoading={isLoading}
+      >
+        Sign Up
+      </Button>
+    </VStack>
+  );
 };
 
 export default SignupForm;
