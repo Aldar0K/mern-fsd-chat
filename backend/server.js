a09515e112fd33397041dcfdc644245375828a85
Aldar <aldarkonov21@gmail.com>
2023-05-26 20:45:26 +0300

    feat: [BE] add socket connection

--- backend/server.js
+++ backend/server.js
@@ -1,6 +1,7 @@
 import colors from "colors";
 import dotenv from "dotenv";
 import express from "express";
+import { Server } from "socket.io";
 import { errorHandler, notFound } from "./middleware/errorMiddleware.js";
 colors;
 
@@ -30,4 +31,48 @@ app.use("/api/message", messageRoutes);
 app.use(notFound);
 app.use(errorHandler);
 
-app.listen(port, console.log(`Server started on port: ${port}`.yellow.bold));
+const server = app.listen(
+  port,
+  console.log(`Server started on port: ${port}`.yellow.bold)
+);
+
+// socket.io
+const io = new Server(server, {
+  pingTimeout: 60000,
+  cors: {
+    origin: "http://localhost:3000",
+    // credentials: true,
+  },
+});
+
+io.on("connection", (socket) => {
+  console.log("Connected to socket.io");
+  socket.on("setup", (userData) => {
+    socket.join(userData._id);
+    socket.emit("connected");
+  });
+
+  socket.on("join chat", (room) => {
+    socket.join(room);
+    console.log("User Joined Room: " + room);
+  });
+  socket.on("typing", (room) => socket.in(room).emit("typing"));
+  socket.on("stop typing", (room) => socket.in(room).emit("stop typing"));
+
+  socket.on("new message", (newMessageRecieved) => {
+    var chat = newMessageRecieved.chat;
+
+    if (!chat.users) return console.log("chat.users not defined");
+
+    chat.users.forEach((user) => {
+      if (user._id == newMessageRecieved.sender._id) return;
+
+      socket.in(user._id).emit("message recieved", newMessageRecieved);
+    });
+  });
+
+  socket.off("setup", () => {
+    console.log("USER DISCONNECTED");
+    socket.leave(userData._id);
+  });
+});
