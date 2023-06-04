22c1713bb85588769c227b7bb5a69abd26ea8e02
Aldar <aldarkonov21@gmail.com>
2023-06-04 14:44:55 +0300

    refactor: [FE] install and configure tailwind

---
+++ frontend/tailwind.config.js
@@ -0,0 +1,10 @@
+/* eslint-disable no-undef */
+
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ['./public/*.html', './src/**/*.{js,jsx,ts,tsx}'],
+  theme: {
+    extend: {}
+  },
+  plugins: []
+};
