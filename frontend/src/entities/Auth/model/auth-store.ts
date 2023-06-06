27f4cd114da73aff70d76e9ce4ee5b7d99f4e24c
Aldar <aldarkonov21@gmail.com>
2023-06-06 21:12:49 +0300

    fix: resolve bug related to auth store

--- frontend/src/entities/Auth/model/auth-store.ts
+++ frontend/src/entities/Auth/model/auth-store.ts
@@ -1,14 +1,12 @@
 import { create } from 'zustand';
 
-import { getToken } from '../lib/helpers/getToken';
-
 export type AuthState = {
   isAuth: boolean;
   setAuth: (isAuth: boolean) => void;
 };
 
 export const useAuthStore = create<AuthState>(set => ({
-  isAuth: !!getToken(),
+  isAuth: false,
   setAuth: (isAuth: boolean) => {
     set({ isAuth });
   }
