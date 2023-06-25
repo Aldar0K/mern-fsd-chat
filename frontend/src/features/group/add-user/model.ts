0efdebea4f07946dd8ea97945cb5273f1ee6260c
Aldar <aldarkonov21@gmail.com>
2023-06-25 16:56:45 +0300

    refactor: [FE] update models for leave and add user group features

--- frontend/src/features/group/add-user/model.ts
+++ frontend/src/features/group/add-user/model.ts
@@ -1,21 +1,26 @@
-import { useMutation } from 'react-query';
+import { useMutation, useQueryClient } from 'react-query';
 
-import { chatApi } from 'entities/chat';
+import { chatApi, chatModel } from 'entities/chat';
 import { useHandleError, useInvalidate, useNotify } from 'shared/lib/hooks';
 
 export const useAddUser = () => {
   const handleError = useHandleError();
   const notify = useNotify();
   const invalidate = useInvalidate();
+  const queryClient = useQueryClient();
 
   return useMutation(
     (addUserDto: chatApi.AddUserDto) => {
       return chatApi.addUser(addUserDto);
     },
     {
-      onSuccess() {
+      onSuccess(updatedChat) {
         notify({ text: 'User added', type: 'success' });
-        invalidate('/chat');
+
+        queryClient.setQueriesData<chatModel.Chat[]>(['/chats'], prevChats =>
+          (prevChats || []).map(chat => (chat._id === updatedChat._id ? updatedChat : chat))
+        );
+        invalidate(['/chats'], { refetchActive: false, refetchInactive: false });
       },
       onError(error) {
         handleError(error);
--- frontend/src/features/group/leave/model.ts
+++ frontend/src/features/group/leave/model.ts
@@ -1,6 +1,7 @@
 import { useMutation, useQueryClient } from 'react-query';
 
 import { chatApi, chatModel } from 'entities/chat';
+import { viewerModel } from 'entities/viewer';
 import { useHandleError, useInvalidate, useNotify } from 'shared/lib/hooks';
 
 export const useRemoveUser = () => {
@@ -8,17 +9,20 @@ export const useRemoveUser = () => {
   const notify = useNotify();
   const invalidate = useInvalidate();
   const queryClient = useQueryClient();
+  const viewer = viewerModel.useViewer();
 
   return useMutation(
     (data: chatApi.RemoveUserDto) => {
       return chatApi.removeUser(data);
     },
     {
-      onSuccess(removedChat) {
+      onSuccess(updatedChat, variables) {
         notify({ text: 'User removed', type: 'success' });
 
         queryClient.setQueriesData<chatModel.Chat[]>(['/chats'], prevChats =>
-          (prevChats || []).filter(chat => chat._id !== removedChat._id)
+          variables.userId === viewer?._id
+            ? (prevChats || []).filter(chat => chat._id !== updatedChat._id)
+            : (prevChats || []).map(chat => (chat._id === updatedChat._id ? updatedChat : chat))
         );
         invalidate(['/chats'], { refetchActive: false, refetchInactive: false });
       },
