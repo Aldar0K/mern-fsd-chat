de88a2ebbf2938294174ff9e707e0cbe21894451
Aldar <aldarkonov21@gmail.com>
2023-06-26 18:50:17 +0300

    refactor: [FE] rename leave group feature to remove-user

--- frontend/src/features/group/index.ts
+++ frontend/src/features/group/index.ts
@@ -1,4 +1,4 @@
 export { AddUserForm, useAddUser } from './add-user';
 export { AddGroupModal } from './create';
-export { GroupUsers, LeaveGroupButton, useRemoveUser } from './leave';
+export { GroupUsers, LeaveGroupButton, useRemoveUser } from './remove-user';
 export { RenameGroupForm, useRenameChat } from './rename';
