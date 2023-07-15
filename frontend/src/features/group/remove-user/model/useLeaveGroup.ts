a1c6164cc44e93dfc66572270cf4da3a96560f50
Aldar <aldarkonov21@gmail.com>
2023-07-15 15:42:35 +0300

    feat: [FE] add useLeaveGroup hook, update leave group button in the remove user feature

--- frontend/src/features/group/remove-user/model/useLeaveGroup.ts
+++ frontend/src/features/group/remove-user/model/useLeaveGroup.ts
@@ -1,3 +1,33 @@
+import { useMutation, useQueryClient } from 'react-query';
+
+import { chatApi, chatModel } from 'entities/chat';
+import { useHandleError, useInvalidate, useNotify } from 'shared/lib/hooks';
+
 export const useLeaveGroup = () => {
-  return;
+  const handleError = useHandleError();
+  const notify = useNotify();
+  const invalidate = useInvalidate();
+  const queryClient = useQueryClient();
+  const setSelectedChat = chatModel.useChatStore(state => state.setSelectedChat);
+
+  return useMutation(
+    (data: chatApi.RemoveUserDto) => {
+      return chatApi.removeUser(data);
+    },
+    {
+      onSuccess(updatedChat) {
+        notify({ text: 'You left the group', type: 'success' });
+
+        queryClient.setQueriesData<chatModel.Chat[]>(['/chats'], prevChats =>
+          (prevChats || []).filter(chat => chat._id !== updatedChat._id)
+        );
+        invalidate(['/chats'], { refetchActive: false, refetchInactive: false });
+
+        setSelectedChat(null);
+      },
+      onError(error) {
+        handleError(error);
+      }
+    }
+  );
 };
--- frontend/src/features/group/remove-user/ui/LeaveGroupButton.tsx
+++ frontend/src/features/group/remove-user/ui/LeaveGroupButton.tsx
@@ -1,41 +1,24 @@
 import { Button } from '@chakra-ui/react';
 import { FC } from 'react';
-import { shallow } from 'zustand/shallow';
 
 import { chatModel } from 'entities/chat';
 import { viewerModel } from 'entities/viewer';
-import { useRemoveUser } from '../model';
-
-const selector = (state: chatModel.ChatState) => ({
-  selectedChat: state.selectedChat,
-  setSelectedChat: state.setSelectedChat
-});
+import { useLeaveGroup } from '../model';
 
 const LeaveGroupButton: FC = () => {
   const viewer = viewerModel.useViewer();
-  const { selectedChat, setSelectedChat } = chatModel.useChatStore(selector, shallow);
-  const { mutateAsync: removeUserMutate, isLoading: removeUserLoading } = useRemoveUser();
+  const selectedChat = chatModel.useChatStore(state => state.selectedChat);
+  const { mutateAsync: leaveGroupMutate, isLoading: leaveGroupLoading } = useLeaveGroup();
 
   const leaveGroup = () => {
     if (!selectedChat || !viewer) return;
-
-    removeUserMutate(
-      {
-        chatId: selectedChat._id,
-        userId: viewer._id
-      },
-      {
-        onSuccess() {
-          setSelectedChat(null);
-        }
-      }
-    );
+    leaveGroupMutate({ chatId: selectedChat._id, userId: viewer._id });
   };
 
   if (!viewer) return null;
 
   return (
-    <Button onClick={leaveGroup} colorScheme='red' disabled={removeUserLoading}>
+    <Button onClick={leaveGroup} colorScheme='red' disabled={leaveGroupLoading}>
       Leave Group
     </Button>
   );
